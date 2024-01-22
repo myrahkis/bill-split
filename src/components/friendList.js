fdad76dfa39d31c77ad0f938455771645b5d623b
Mira <kruzinka@yandex.ru>
2024-01-22 21:26:27 +0300

    friend list but make it pretty

--- src/components/friendList.js
+++ src/components/friendList.js
@@ -1,17 +1,70 @@
 import React from "react";
-import './friendList.css';
+import "./friendList.css";
 
-function FriendList({ img, name, balance }) {
-    return (
-        <div className="friend-wrapper">
-            <img src={img} alt="pfp" />
-            <div className="info-wrapper">
-                <p>{name}</p>
-                <p>{balance}</p>
-            </div>
-            <button className="select-btn">Select</button>
-        </div>
-    );
+const friendsInit = [
+  {
+    id: 123456,
+    name: "Judy",
+    img: "icons/judy.png",
+    balance: -10,
+  },
+  {
+    id: 376395,
+    name: "Grace",
+    img: "icons/grace.png",
+    balance: 0,
+  },
+  {
+    id: 874686,
+    name: "Lorenzo",
+    img: "icons/lorenzo.png",
+    balance: 40,
+  },
+];
+
+function FriendList() {
+  return (
+    <div className="friend-list-wrapper">
+      {friendsInit.map((friend) => (
+        <Friend
+          key={friend.id}
+          img={friend.img}
+          name={friend.name}
+          balance={friend.balance}
+        />
+      ))}
+      <div className="float-right">
+        <button className="add-btn">Add a friend</button>
+      </div>
+    </div>
+  );
+}
+
+function Friend({ img, name, balance }) {
+  return (
+    <div className="friend-wrapper">
+      <img src={img} alt="pfp" />
+      <div className="info-wrapper">
+        <h4>{name}</h4>
+        <p
+          className={
+            balance < 0
+              ? "balance-red"
+              : balance > 0
+              ? "balance-green"
+              : "balance-even"
+          }
+        >
+          {balance < 0
+            ? `You own ${name} $${Math.abs(balance)}`
+            : balance > 0
+            ? `${name} owns you $${balance}`
+            : `You and ${name} are even`}
+        </p>
+      </div>
+      <button className="select-btn">Select</button>
+    </div>
+  );
 }
 
 export default FriendList;
